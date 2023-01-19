985ea3bdbb99dd5927593ca1932c907d046bc794
Fernando Moreno <fercano741@hotmail.com>
2023-01-18 22:34:38 -0300

    Update movieModel

--- api/app/Models/Movie.php
+++ api/app/Models/Movie.php
@@ -8,7 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+    protected $fillable = ['titulo','descripcion','fecha_de_estreno','portada'];
     public function comments(){
         return $this->hasMany(Comment::class, 'id');
+
     }
 }
